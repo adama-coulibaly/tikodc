a4f249b22e206d63f95bda7733867e4f96568c6f
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-11-02 15:38:38 +0000

    Slider configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,196 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
+
+//  ICI  C'EST LA CLASSE PRINCIPALE MAIN
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}):super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'TikODC',
+      debugShowCheckedModeBanner: false,
+      home: menuBottom(),
+      
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+//  Menu d'en bas 
+class menuBottom extends StatefulWidget {
+  const menuBottom({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<menuBottom> createState() => _menuBottomState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// Ici on declare les affichages par rapport aux differents cliques
 
-  void _incrementCounter() {
+class _menuBottomState extends State<menuBottom> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body:Accueil(),
+      bottomNavigationBar: BottomNavigationBar(
+        items:  <BottomNavigationBarItem>[   
+
+// ICI ON DECLARE LES MENUS DE NOTRE BOTTOM_NAV_BAR
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flash_on),
+            label: 'Now',
+          ),
+          BottomNavigationBarItem(
+           icon: Image.asset('assets/images/tiktok_add.png',height: 25,
+           ),
+           label: '',
+          
+           
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.comment_outlined),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profil',
+          ),
+         
+        ],
+        currentIndex: _selectedIndex,
+
+        // //Les propietés pour pouvoir modifier les icones : BottomNavBar
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xFF141518),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+
+        onTap: _onItemTapped,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// LA page d'accueil
+
+class Accueil extends StatelessWidget{
+   Accueil({Key? key}) :super(key: key);
+
+final List<Map> tikodcItems = [
+  {
+    "video":"assets/videos/video_1.mp4",
+  },
+  {
+    "video":"assets/videos/video_2.mp4",
+  },
+  {
+    "video":"assets/videos/video_3.mp4",
+  },
+  {
+    "video":"assets/videos/video_4.mp4",
+  },
+    {
+    "video":"assets/videos/video_5.mp4",
+  },
+    {
+    "video":"assets/videos/video_6.mp4",
+  },
+];
+
+
+  @override
+  Widget build(BuildContext context){
+    return CarouselSlider(
+  options: CarouselOptions(
+    height: double.infinity,
+    scrollDirection: Axis.vertical,
+    viewportFraction: 1.0,
+    
+    ),
+  items: tikodcItems.map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          color: Color(0xFF141518),
+          child: Stack(
+            children: [VideoWidgets(videoUrl:i['video']),
+            const PostContent()
+            ],
+          ),
+        );
+      },
+    );
+  }).toList(),
+);
+  }
+}
+
+// MES VIDEOS
+
+class VideoWidgets extends StatefulWidget {
+  const VideoWidgets({Key? key, required this.videoUrl}) : super(key: key);
+  final String videoUrl;
+
+  @override
+  _VideoWidgetsState createState() => _VideoWidgetsState(this.videoUrl);
+}
+
+class _VideoWidgetsState extends State<VideoWidgets> {
+  late VideoPlayerController _controller;
+  final String videoUrl;
+  _VideoWidgetsState(this.videoUrl);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(videoUrl)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        _controller.play();
+        setState(() {});
+      });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  return VideoPlayer(_controller);
+  }
+  }
+
+  // Les contenus ici 
+
+  class PostContent extends StatelessWidget{
+    const PostContent({Key? key}):super(key: key);
+
+    @override
+    Widget build(BuildContext buildContext){
+      return Column(
+        children: [
+          Container(
+            height: 100,
+            color: Colors.blue,
+          ),
+          Expanded(child: Container(
+            color: Colors.red,
+          ))
+        ],
+      ); 
+    }
+  }
